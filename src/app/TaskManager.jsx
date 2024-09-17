a537213e899996efbbde19cce0e6b62c1e96b269
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-09-17 16:12:44 +0530

    set the value of the input as inputValue variable and validate the input feeld

--- src/app/TaskManager.jsx
+++ src/app/TaskManager.jsx
@@ -1,8 +1,18 @@
 import {useState } from "react";
 
 function TaskManager(){
+    // store the tasks and display
     const [tasks, setTask] = useState([]);
+    // take value from input
     const [inputValue, setInputValue] = useState("");
+
+    // store data when click the Add button
+    function addData(){
+        // validating the input
+        inputValue.length==0?setTask([...tasks]):setTask([...tasks, inputValue]);
+        // set the value to empty after add the task
+        setInputValue("")
+    }
     return (
         <>
             <h1> Task Manager </h1>
@@ -12,8 +22,8 @@ function TaskManager(){
                 )}
             </ul>
             <div>
-                <input type="text" onChange={(even) => setInputValue (even.target.value)}/>
-                <button>Add</button>
+                <input value={inputValue} type="text" onChange={(even) => setInputValue (even.target.value)}/>
+                <button onClick={addData}>Add</button>
             </div>
         </>
     )  
